60ba6e9b85e98163d40af653b1f74382dc7caec7
Hyungjin Jung <jhj301@gmail.com>
2026-09-04 13:54:00 +0900

    feat: shared PlayHub mute toggle and WebAudio unlock

--- docs/games/sliding/game.js
+++ docs/games/sliding/game.js
@@ -102,7 +102,7 @@
       let inv = 0;
       for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
-          if (arr[i] && arr[j] && arr[i] > arr[j]) inv++) inv++;
+          if (arr[i] && arr[j] && arr[i] > arr[j]) inv++;
         }
       }
       return inv;
